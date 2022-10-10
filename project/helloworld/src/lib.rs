5b8efa706ba8e688a083a2a15ea1d1d8ac78530b
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2022-10-11 00:26:24 +0900

    helloworldクレートにdoc testの練習を追加

--- project/helloworld/src/lib.rs
+++ project/helloworld/src/lib.rs
@@ -1,3 +1,15 @@
+/// cargo doc の練習
+/// 
+/// 対象の関数の上に記述する必要あり。
+/// 
+/// また、書き方がMarkdown形式であり、空行とインデントが必要。
+/// 
+///     helloworld::helloworld();
+/// 
+/// また、cargo test によるテストも可能。
+/// 
+///     assert!(true);
+
 pub fn helloworld() {
     // その他のクレートから実行できるようにpubを付けた。
     println!("Hello World.");
--- project/helloworld/tests/test.rs
+++ project/helloworld/tests/test.rs
@@ -1,5 +1,5 @@
 #[test]
-fn tset_hello() {
+fn tsets_helloworld() {
     // helloworldクレート(このファイルがあるクレート)のhelloworld()関数を実行。
     // testsディレクトリでの同一クレートの関数呼び出しは、他のクレートからの呼び出しのように記述する。
     helloworld::helloworld();
